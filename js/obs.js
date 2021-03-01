a4be4f2e3f6664d95e8ad9ed6677311baad0f9c7
opatut <opatut@users.noreply.github.com>
2021-03-01 21:08:03 +0000

    deploy: ae7d273c674528b0aaa33c0d76572e03c5864584

---
+++ js/obs.js
@@ -0,0 +1,49 @@
+(() => {
+  // js/obs.js
+  function replaceWithIframe(div, videoId) {
+    let iframe = document.createElement("iframe");
+    iframe.setAttribute("src", "https://www.youtube.com/embed/" + div.dataset.videoId + "?autoplay=1&rel=0");
+    iframe.setAttribute("frameborder", "0");
+    iframe.setAttribute("allowfullscreen", "1");
+    iframe.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture");
+    while (div.childNodes.length) {
+      div.removeChild(div.childNodes[0]);
+    }
+    div.appendChild(iframe);
+  }
+  function initYouTubeVideos() {
+    let wrappers = document.getElementsByClassName("youtube-video-wrapper");
+    for (let n = 0; n < wrappers.length; n++) {
+      let wrapper = wrappers[n];
+      let videoId = wrapper.dataset.videoId;
+      wrapper.onclick = function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        replaceWithIframe(wrapper, videoId);
+      };
+    }
+  }
+  document.addEventListener("DOMContentLoaded", initYouTubeVideos);
+  function initSliders() {
+    let wrappers = document.getElementsByClassName("slider");
+    for (let n = 0; n < wrappers.length; n++) {
+      let wrapper = wrappers[n];
+      if (!wrapper.id) {
+        wrapper.setAttribute("id", "slider-" + n);
+      }
+      let slider = new IdealImageSlider.Slider({
+        selector: "#" + wrapper.id,
+        transitionDuration: 250,
+        effect: "fade",
+        captions: true,
+        bullets: true,
+        height: "'4:3'"
+      });
+      if (slider._attributes) {
+        slider.addBulletNav();
+        slider.addCaptions();
+      }
+    }
+  }
+  document.addEventListener("DOMContentLoaded", initSliders);
+})();
