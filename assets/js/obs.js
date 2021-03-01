cf14c07cedf4b1965e09faf9c2638a43b8cda79f
Paul Bienkowski <pb@opatut.de>
2021-03-01 16:23:15 +0100

    Fix homepage youtube videos and container styles

--- assets/js/obs.js
+++ assets/js/obs.js
@@ -5,11 +5,15 @@
  */
 
 function replaceWithIframe(div, videoId) {
-  var iframe = document.createElement('iframe')
+  let iframe = document.createElement('iframe')
   iframe.setAttribute('src', 'https://www.youtube.com/embed/' + div.dataset.videoId + '?autoplay=1&rel=0')
   iframe.setAttribute('frameborder', '0')
   iframe.setAttribute('allowfullscreen', '1')
-  iframe.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture')
+  iframe.setAttribute(
+    'allow',
+    'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+  )
+
   while (div.childNodes.length) {
     div.removeChild(div.childNodes[0])
   }
@@ -17,10 +21,10 @@ function replaceWithIframe(div, videoId) {
 }
 
 function initYouTubeVideos() {
-  var wrappers = document.getElementsByClassName('youtube-video-wrapper')
-  for (var n = 0; n < wrappers.length; n++) {
-    var wrapper = wrappers[n]
-    var videoId = wrapper.dataset.videoId
+  let wrappers = document.getElementsByClassName('youtube-video-wrapper')
+  for (let n = 0; n < wrappers.length; n++) {
+    let wrapper = wrappers[n]
+    let videoId = wrapper.dataset.videoId
 
     wrapper.onclick = function (e) {
       e.preventDefault()
@@ -35,15 +39,15 @@ document.addEventListener('DOMContentLoaded', initYouTubeVideos)
 //////////////////////////////////////////////
 
 function initSliders() {
-  var wrappers = document.getElementsByClassName('slider')
-  for (var n = 0; n < wrappers.length; n++) {
-    var wrapper = wrappers[n]
+  let wrappers = document.getElementsByClassName('slider')
+  for (let n = 0; n < wrappers.length; n++) {
+    let wrapper = wrappers[n]
 
     if (!wrapper.id) {
       wrapper.setAttribute('id', 'slider-' + n)
     }
 
-    var slider = new IdealImageSlider.Slider({
+    let slider = new IdealImageSlider.Slider({
       selector: '#' + wrapper.id,
       transitionDuration: 250,
       effect: 'fade',
